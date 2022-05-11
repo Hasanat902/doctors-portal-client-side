c23301350f718e04681dd8b52f1e7504e249f962
Hasanat902 <abulhasanat902@gmail.com>
2022-05-11 12:34:54 +0600

    appoinment page created

--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -11,8 +11,8 @@ const MakeAppointment = () => {
             </div>
             <div className='flex-1 pr-14'>
                 <h2 className='text-xl text-primary font-bold'>Appointment</h2>
-                <h3 className='text-3xl text-white'>Make an appointment Today</h3>
-                <p className='text-white'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <h3 className='text-3xl text-white py-5'>Make an appointment Today</h3>
+                <p className='text-white pb-5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                 <PrimaryButton>Get Started</PrimaryButton>
             </div>
         </section>
