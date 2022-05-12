f7f53009a34f7fabce00ba0f727000f2199e77c2
Hasanat902 <abulhasanat902@gmail.com>
2022-05-13 00:31:56 +0600

    create signup form and also implement signout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import SignUp from './Pages/Login/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/appointment' element={<Appointment></Appointment>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,22 +1,42 @@
 import React from "react";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
 import { useForm } from "react-hook-form";
+import Loading from "../../Shared/Loading";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
 
   const { register, formState: { errors }, handleSubmit } = useForm();
 
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  let signInError;
+
+  if(loading || gLoading){
+      return <Loading></Loading>
+  }
+
+  if(error || gError){
+      signInError = <p className="text-red-600 mb-3">{error?.message || gError?.message}</p>
+  }
+
   const onSubmit = data => {
     console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
   }
 
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
-          <h2 className="text-center text-2xl font-bold">LogIn</h2>
+          <h2 className="text-center text-2xl font-bold">Log In</h2>
           <form onSubmit={handleSubmit(onSubmit)}>
           <div class="form-control w-full max-w-xs">
             <label class="label">
@@ -69,8 +89,10 @@ const Login = () => {
             </label>
             </div>
             
-            <input className="btn w-full max-w-xs text-white" type="submit" value="LogIn" />
+            {signInError}
+            <input className="btn w-full max-w-xs text-white" type="submit" value="Log In" />
           </form>
+          <p><small>New to Doctors Portal? <Link to="/signup" className="text-primary">Create New Account</Link></small></p>
           <div className="divider">OR</div>
           <button
             onClick={() => signInWithGoogle()}
