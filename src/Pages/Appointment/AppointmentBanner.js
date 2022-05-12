c640a4cdb24e53696a35bbbb37e46789e937f42d
Hasanat902 <abulhasanat902@gmail.com>
2022-05-12 21:44:13 +0600

    create login form and google signin

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import chair from '../../assets/images/chair.png';
-import bg from '../../assets/images/bg.png';
-import { DayPicker } from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
+import React from "react";
+import chair from "../../assets/images/chair.png";
+import bg from "../../assets/images/bg.png";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 
-const AppointmentBanner = ({date, setDate}) => {
-
-
-    return (
-        <div style={{background: `url(${bg})`, backgroundSize: 'cover'}} class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-md rounded-lg shadow-2xl" alt='Dentist Chair' />
-                <div>
-                    <DayPicker
-                    mode="single"
-                    selected={date}
-                    onSelect={setDate}
-                    ></DayPicker>
-                </div>
-            </div>
+const AppointmentBanner = ({ date, setDate }) => {
+  return (
+    <div
+      style={{ background: `url(${bg})`, backgroundSize: "cover" }}
+      className="hero min-h-screen"
+    >
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={chair}
+          className="max-w-md rounded-lg shadow-2xl"
+          alt="Dentist Chair"
+        />
+        <div>
+          <DayPicker
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+          ></DayPicker>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AppointmentBanner;
