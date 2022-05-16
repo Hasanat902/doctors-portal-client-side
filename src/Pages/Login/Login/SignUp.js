b5b467d3c9599951eb45ff08d4aa7e1ed66c6a59
Hasanat902 <abulhasanat902@gmail.com>
2022-05-16 12:09:46 +0600

    added admin role

--- src/Pages/Login/Login/SignUp.js
+++ src/Pages/Login/Login/SignUp.js
@@ -8,6 +8,7 @@ import auth from "../../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../../Shared/Loading";
 import { Link, useNavigate } from "react-router-dom";
+import useToken from "../../../hooks/useToken";
 
 const SignUp = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -22,6 +23,8 @@ const SignUp = () => {
     useCreateUserWithEmailAndPassword(auth);
 
   const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+  const [token] = useToken(user || gUser);
+
   const navigate = useNavigate();
 
   let signInError;
@@ -38,11 +41,15 @@ const SignUp = () => {
     );
   }
 
+  if(token) {
+     navigate("/appointment");
+  }
+
   const onSubmit = async (data) => {
     await createUserWithEmailAndPassword(data.email, data.password);
     await updateProfile({ displayName: data.name });
     console.log("Update done");
-    navigate("/appointment");
+    
   };
 
   return (
